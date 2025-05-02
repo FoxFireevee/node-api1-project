c3923ea406dbc8596198e49c2918f9bf64fe19b6
Hailey Murphy <foxfireevee@gmail.com>
2025-05-02 17:23:53 -0400

    Completed this very fun project

--- api/server.js
+++ api/server.js
@@ -1,3 +1,113 @@
 // BUILD YOUR SERVER HERE
+// Imports
+const express = require('express')
+const Model = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// Instance of Express
+const server = express()
+
+// Global Middlware
+server.use(express.json())
+
+// ENDPOINTS
+// Endpoint: Find
+server.get('/api/users', async (req, res) => {
+    try {
+        const users = await Model.find()
+        res.status(200).json(users)
+    } catch(err) {
+        res.status(500).json({ 
+            message: `Error Fetching Users: ${err.message}`
+        })
+    }
+})
+
+// Endpoint: Find By ID
+server.get('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const user = await Model.findById(id)
+        if(!user) {
+            res.status(404).json({
+                message: 'does not exist'
+            })
+        } else {
+            res.status(200).json(user)
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: `Error Fetching User: ${err.message}`
+        })
+    }
+})
+
+// Endpoint: Insert
+server.post('/api/users', async (req, res) => {
+    try {
+        const { name, bio } = req.body
+        if(!name || !bio) {
+            res.status(400).json({
+                message: `provide name and bio`
+            })
+        } else {
+            const createdUser = await Model.insert({name, bio})
+            res.status(201).json(createdUser)
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: `Could Not Post New User: ${err.message}`
+        })
+    }
+})
+
+// Endpoint: Update
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const { name, bio } = req.body
+        if(!name || !bio) {
+            res.status(400).json({
+                message: 'provide name and bio'
+            })
+        } else {
+            const updatedUser = await Model.update(id, {name, bio})
+            if(!updatedUser) {
+                res.status(404).json({
+                    message: 'This user does not exist'
+                })
+            } else {
+                res.status(200).json(updatedUser)
+            }
+            
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: `Updating the user went wrong: ${err.message}`
+        })
+    }
+})
+
+// Endpoint: Remove
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const deletedUser = await Model.remove(id)
+        if(!deletedUser) {
+            res.status(404).json({
+                message: 'does not exist'
+            })
+        } else {
+            res.status(200).json(deletedUser)
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: `Something went wrong when deleting the user: ${err.message}`
+        })
+    }
+})
+
+// Endpoint: Reset Data Base
+
+
+// Exposing The Server
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const server = require('./api/server');
 
-const port = 9000;
+const port = 3003;
 
 // START YOUR SERVER HERE
+server.listen(port, () => {
+    console.log("Server started on http://localhost:3003")
+})
